2a644a8011e4e86617999a81aa1141f66b978168
yj <bit-user@bit>
2016-10-10 16:34:31 +0900

    commit

---
+++ src/HRSalary.java
@@ -0,0 +1,72 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+public class HRSalary {
+	public static void main(String[] args) {
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		Scanner sc = new Scanner(System.in);
+		try {
+			// 1, jdbc 드라이버 로딩(oracle)
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+
+			// 2,
+			String url = "jdbc:oracle:thin:@localhost:1521:xe";
+
+			conn = DriverManager.getConnection(url, "hr", "hr");
+
+			// 3, Statement 객체 생성
+			stmt = conn.createStatement();
+
+			System.out.print("최소 봉급 :");
+			int scan1 = sc.nextInt();
+			System.out.print("최대 봉급 :");
+			int scan2 = sc.nextInt();
+
+			// 4, SQL문 생성
+
+			String sql = "select FIRST_NAME, LAST_NAME, salary from employees where " + scan1 + "<salary and salary < "
+					+ scan2 + "";
+			rs = stmt.executeQuery(sql);
+			// 5, 데이터 결과 가져오기(ResultSet)
+
+			System.out.println("이름      	:성    	:급여");
+			System.out.println("--------------------------------------------------------------");
+			while (rs.next()) {
+				String firstName = rs.getString(1);
+				String lastName = rs.getString(2);
+				Long salary = rs.getLong(3);
+				System.out.println(firstName + "      	:" + lastName + "    	:" + salary);
+			}
+
+		} catch (ClassNotFoundException e) {
+			System.out.println("드라이버로딩 실패 : " + e);
+		} catch (SQLException e) {
+			System.out.println("error: " + e);
+		} finally {
+			try {
+				// 3, 자원 정리
+				if (sc != null) {
+					sc.close();
+				}
+				if (rs != null) {
+					rs.close();
+				}
+				if (stmt != null) {
+					stmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
